41f14c9f96e576f60b8416826af560cde6657426
Abrar Hussain <ah3608@gmail.com>
2020-12-28 22:09:32 +0500

    Program-01 added to covert Celsius into Fahrenheit

---
+++ Program01/program01.js
@@ -0,0 +1,8 @@
+var value1 = 9;
+var value2 = 5;
+var value3 = 32;
+var spaceHolder = "";
+var userInput =  prompt ("Write a Temperature in Celsius to convert it into Fahrenheit" );
+if (userInput >= 0){
+    alert (value1 / value2 * userInput + value3 + " Fahrenhite ")
+}
